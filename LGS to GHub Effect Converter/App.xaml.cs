732d032fab6f445afbd04280bea45e481b090109
Danealor <iamblonder@gmail.com>
2018-09-08 11:47:45 -0400

    Add project files.

---
+++ LGS to GHub Effect Converter/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LGS_to_GHub_Effect_Converter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
